7c6136cd6db0f830ad4d769e3770f904281298a2
felipe-monteiro16 <felipemmateuss@gmail.com>
2025-04-03 14:14:04 -0300

    Implement Splitwise API integration and user authorization flow

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 from dotenv import load_dotenv
 import os
+from splitwise import Splitwise
+#import logging
 
+
+def pause():
+    input("\nPress Enter to continue...")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Getting the environment variables from the .env file
 load_dotenv()
 
 consumer_key = os.getenv("CONSUMER_KEY")
 consumer_secret = os.getenv("CONSUMER_SECRET")
 api_key = os.getenv("API_KEY")
-print(consumer_key, consumer_secret, api_key) # prints the value of the secret key
+
+# Initialize the Splitwise client
+client = Splitwise(consumer_key, consumer_secret, api_key=api_key)
+
+# Get the authorization URL
+url, oauth_token_secret = client.getAuthorizeURL()
+
+# Print the URL to the console
+print("Please visit this URL to authorize the application: ", url)
+oauth_token = input("Enter the oauth_token from the URL: ")
+oauth_verifier = input("Enter the oauth_verifier from the URL: ")
+pause()
+
+
+# Get the access token
+access_token = client.getAccessToken(
+    oauth_token, oauth_token_secret, oauth_verifier
+    )
+current_user = client.getCurrentUser()
+print("Access token: ", access_token)
+print("Current user: ", current_user)
