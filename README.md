d7090c4a760bb1628af947cc16796b9e903e54f3
felipe-monteiro16 <felipemmateuss@gmail.com>
2025-04-09 15:59:43 -0300

    Update .gitignore, README.md and enhance main.py for access token management.
    Solved Issues
    #1 - Add Validation for var envs
    #2 - Improve auth process
    #4 - Create command to get user

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ poetry install
 ```
 ### File `.env`
 
-In the project root, create an `.env` file to store the Splitwise API keys.
+In the project root, create an `.env` file to store the Splitwise API keys, you can get these keys from the project administrators.
 
 The `.env` file have to contain this following environment variables.
 
@@ -28,3 +28,39 @@ CONSUMER_SECRET=consumer_secret_here
 API_KEY=api_key_here
 ```
 
+After these steps, your project will be ready to run.
+
+# Running the project
+
+To get the access of the `Splitwise API`, it will be need to do some steps on first run.
+
+In the project root, open the terminal and run:
+```
+poetry run main.py
+```
+
+Temporary: If it gets error, try:
+```
+poetry run main.py --no-root
+```
+
+### First Running
+
+
+After running, on browser, open the link that appeared on terminal and authorize the access.
+
+After authorizing, click on the text:
+
+`Click to show out of band authentication information`
+
+Copy the `oauth_verifier` that appears:
+
+```
+params[:oauth_verifier] is <oauth_verifier_here>
+``` 
+
+And paste on terminal.
+
+The tokens will be saved on `access_token.json` file. If these tokens expire, you'll need to do this process again to keep the acess of the `Splitwise API`.
+
+Done!
--- main.py
+++ main.py
@@ -1,38 +1,99 @@
-from dotenv import load_dotenv
+"""Splitwise API Python Client"""
+import json
 import os
+from dotenv import load_dotenv
 from splitwise import Splitwise
-#import logging
+from splitwise.exception import SplitwiseUnauthorizedException
 
 
-def pause():
+def pause() -> None:
+    """Pause the execution of the program until the user presses Enter."""
     input("\nPress Enter to continue...")
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-# Getting the environment variables from the .env file
-load_dotenv()
+def get_access_token() -> str:
+    """Get the access token from the user.
+    The user will be redirected to a URL to authorize the application."""
 
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-api_key = os.getenv("API_KEY")
+    # Get the authorization URL
+    url, oauth_token_secret = client.getAuthorizeURL()
+
+    # Print the URL to the console
+    print("Please visit this URL to authorize the application: ", url)
+    oauth_token = url.split("oauth_token=")[-1]
+    oauth_verifier = input("Enter the oauth_verifier from the URL: ")
+    pause()
+    access_token = client.getAccessToken(
+        oauth_token, oauth_token_secret, oauth_verifier
+    )
+    save_access_token(access_token)
+
+
+def save_access_token(token: str) -> None:
+    """Save the access token to a file."""
+    with open("access_token.json", "w", encoding="utf-8") as f:
+        json.dump(token, f)
+
+
+def load_access_token() -> str:
+    """Load the access token from a file."""
+    # Check if the file exists
+    if os.path.exists("access_token.json"):
+        with open("access_token.json", "r", encoding="utf-8") as f:
+            token = json.load(f)
+            return token
+    else:
+        print("Access token file not found.")
+        return None
+
+
+def initialize_client() -> str:
+    """Initialize the Splitwise client."""
+    # Getting the environment variables from the .env file
+    load_dotenv()
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    api_key = os.getenv("API_KEY")
+    # Check if the environment variables are set
+    if not consumer_key or not consumer_secret or not api_key:
+        print("Please set the environment variables. More details on README.md")
+        return None
+
+    # Set the Splitwise client
+    clt = Splitwise(consumer_key, consumer_secret, api_key=api_key)
+    return clt
+
 
 # Initialize the Splitwise client
-client = Splitwise(consumer_key, consumer_secret, api_key=api_key)
+client = initialize_client()
+if client is not None:
+    print("Client initialized successfully.")
+    # Testing...
+    CONDICTION = os.path.exists("access_token.json")
+    if CONDICTION:
+        acess_token = load_access_token()
+        client.setAccessToken(acess_token)
+    else:
+        get_access_token()
+else:
+    print("Client initialization failed.")
 
-# Get the authorization URL
-url, oauth_token_secret = client.getAuthorizeURL()
 
-# Print the URL to the console
-print("Please visit this URL to authorize the application: ", url)
-oauth_token = input("Enter the oauth_token from the URL: ")
-oauth_verifier = input("Enter the oauth_verifier from the URL: ")
-pause()
+# Usage example
+
+try:
+    # Get the current user
+    current_user = client.getCurrentUser()
+    print("User ID: ", current_user.id)
+    print("User Name: ", current_user.first_name)
 
 
-# Get the access token
-access_token = client.getAccessToken(
-    oauth_token, oauth_token_secret, oauth_verifier
-    )
-current_user = client.getCurrentUser()
-print("Access token: ", access_token)
-print("Current user: ", current_user)
+
+except SplitwiseUnauthorizedException as e:
+    print("Access token is invalid.")
+    # Reauthorize the application
+    get_access_token()
+    print("Access token updated successfully.")
+    print("Please run the program again.")
+    pause()
